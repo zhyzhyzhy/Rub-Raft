13604097167d77a2d4481919371bb90470bd0ca2
zhyzhyzhy <1012146386@qq.com>
2018-11-17 23:19:48 +0800

    更新rpc provider

--- rpc/src/main/java/cc/lovezhy/raft/rpc/RpcProvider.java
+++ rpc/src/main/java/cc/lovezhy/raft/rpc/RpcProvider.java
@@ -1,25 +1,26 @@
 package cc.lovezhy.raft.rpc;
 
+import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
 import java.util.Map;
 
 public class RpcProvider {
 
     private static final Logger log = LoggerFactory.getLogger(RpcProvider.class);
 
-    public static RpcProvider create(Class<?> providerClazz) {
+    static RpcProvider create(Class<?> providerClazz) {
         return new RpcProvider(providerClazz);
     }
 
     private Object instance;
-    private Map<String, Method> methodMap = new HashMap<>();
+    private Map<String, Method> methodMap;
 
     private RpcProvider(Class<?> providerClazz) {
+        this.methodMap = Maps.newHashMap();
 
         for (Method method : providerClazz.getDeclaredMethods()) {
             methodMap.put(method.getName(), method);
--- rpc/src/main/java/cc/lovezhy/raft/rpc/RpcServer.java
+++ rpc/src/main/java/cc/lovezhy/raft/rpc/RpcServer.java
@@ -13,7 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
