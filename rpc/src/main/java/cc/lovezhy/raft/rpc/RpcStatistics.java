fdd14b321bc27980d4d2b3db48c62b7aaa5039ef
zhyzhyzhy <1012146386@qq.com>
2019-01-25 21:55:27 +0800

    给RpcServer增加一个HttpService用来提供一些统计信息

--- rpc/src/main/java/cc/lovezhy/raft/rpc/RpcStatistics.java
+++ rpc/src/main/java/cc/lovezhy/raft/rpc/RpcStatistics.java
@@ -1,4 +1,35 @@
 package cc.lovezhy.raft.rpc;
 
+import io.vertx.core.json.JsonObject;
+
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicLong;
+
+@ThreadSafe
 public class RpcStatistics {
+
+    public static RpcStatistics create() {
+        return new RpcStatistics();
+    }
+
+    /**
+     * 进来的Rpc个数
+     */
+    private static final String INCOMING_TOTAL_REQUEST = "incomingTotalRequest";
+    private AtomicLong incomingTotalRequest;
+
+    private RpcStatistics() {
+        this.incomingTotalRequest = new AtomicLong(0L);
+    }
+
+    public void incrIncomingRequestAsync() {
+        CompletableFuture.runAsync(() -> incomingTotalRequest.incrementAndGet());
+    }
+
+    public JsonObject toJsonObject() {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.put(INCOMING_TOTAL_REQUEST, incomingTotalRequest);
+        return jsonObject;
+    }
 }
--- rpc/src/main/java/cc/lovezhy/raft/rpc/resource/RpcServerStateResource.java
+++ rpc/src/main/java/cc/lovezhy/raft/rpc/resource/RpcServerStateResource.java
@@ -1,6 +1,63 @@
-package cc.lovezhy.raft.rpc;
+package cc.lovezhy.raft.rpc.resource;
 
-public class RpcServerStateResource {
+import cc.lovezhy.raft.rpc.RpcStatistics;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 
+public class RpcServerStateResource extends AbstractVerticle {
+
+    private static final Logger log = LoggerFactory.getLogger(RpcServerStateResource.class);
+
+    public static RpcServerStateResource create(int port, RpcStatistics rpcStatistics) {
+        checkNotNull(rpcStatistics);
+        return new RpcServerStateResource(port, rpcStatistics);
+    }
+
+    private RpcStatistics rpcStatistics;
+
+    private HttpServer httpServer;
+
+    private int port;
+
+    private RpcServerStateResource(int port, RpcStatistics rpcStatistics) {
+        checkNotNull(rpcStatistics);
+        this.port = port;
+        this.rpcStatistics = rpcStatistics;
+    }
+
+    @Override
+    public void start() {
+        vertx = Vertx.vertx();
+        httpServer = vertx.createHttpServer();
+        Router router = Router.router(vertx);
+
+        /**
+         * get rpcServer statistic
+         */
+        router.get("/statistic").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.putHeader(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON);
+            response.end(rpcStatistics.toJsonObject().toString());
+        });
+
+        httpServer.requestHandler(router::accept)
+                .listen(port);
+    }
+
+    public void close() {
+        try {
+            this.httpServer.close();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
 }
